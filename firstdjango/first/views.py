59268ee107188b871494a5ac29b69c6958f43cca
jeiseung <jsjs7043@gmail.com>
2025-03-05 15:38:04 +0900

    떡볶이

--- firstdjango/first/views.py
+++ firstdjango/first/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    context = {'name': '이제승',
-               'name2':'홍길동',
-               'name3':'아무게', }
+    context = {"name": "떡볶이",
+               "hobby": "고추장",
+               "interest": "계란",
+                 }
 
     return render(request, "first/home.html", context)
